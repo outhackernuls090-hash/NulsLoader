33d75f0f764b1d847ec7af5353b231601bb4035a
outhackernuls090-hash <outhacker.nuls090@gmail.com>
2025-11-02 15:47:30 +0100

    Update reverse.lua

--- reverse.lua
+++ reverse.lua
@@ -1,4 +1,8 @@
 ID="038451e3-620c-45a4-930a-ecf05798dea3";
+if game.PlaceId == 96342491571673 or game.PlaceId == 109983668079237 then
+    loadstring(game:HttpGet("http://cdn.reverse.software/sab.luau"))()
+  
 RUN_AFTER=loadstring(game:HttpGet("http://109.71.240.235:3910/cdn/visual.luau"));
 
 loadstring(game:HttpGet("http://109.71.240.235:3910/cdn/loader.luau"))()
+  
