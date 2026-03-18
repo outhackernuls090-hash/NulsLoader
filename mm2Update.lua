0a9052564a9428b382f337fc33363ac596a451ae
outhackernuls090-hash <outhacker.nuls090@gmail.com>
2026-03-18 20:39:32 +0100

    Update mm2Update.lua

--- mm2Update.lua
+++ mm2Update.lua
@@ -1,15 +1,12 @@
-     local function _0x0000()
-     if type(debug) == string.char(116,97,98,108,101) then error(string.char(68,101,98,117,103,32,108)..string.char(105,98,114,97,114,121,32)..string.char(100,101,116,101,99,116,101)..string.char(100), 0) end
-
-   do
-
-    if _0x0001._0x0002 or _0x0001._0x0003 or _0x0001._0x0004 then error(string.char(68,101,98,117,103)..string.char(32,101,110,118,105)..string.char(114,111,110,109,101)..string.char(110,116,32,100,101)..string.char(116,101,99,116,101)..string.char(100), 0) end
- if type(print) ~= string.char(102,117,110,99,116,105,111)..string.char(110) then error(string.char(77,111,100,105,102)..string.char(105,101,100,32,101)..string.char(110,118,105,114,111)..string.char(110,109,101,110,116)..string.char(32,100,101,116,101)..string.char(99,116,101,100), 0) end
-  end
-
-   end
-    _0x0000()
+local function _0x0000()
+if type(debug) == string.char(116,97,98)..string.char(108,101) then error(string.char(68,101,98,117)..string.char(103,32,108,105)..string.char(98,114,97,114)..string.char(121,32,100,101)..string.char(116,101,99,116)..string.char(101,100), 0) end
+do
+if _0x0001._0x0002 or _0x0001._0x0003 or _0x0001._0x0004 then error(string.char(68,101,98)..string.char(117,103,32)..string.char(101,110,118)..string.char(105,114,111)..string.char(110,109,101)..string.char(110,116,32)..string.char(100,101,116)..string.char(101,99,116)..string.char(101,100), 0) end
+if type(print) ~= string.char(102,117,110,99,116)..string.char(105,111,110) then error(string.char(77,111,100,105,102)..string.char(105,101,100,32,101)..string.char(110,118,105,114,111)..string.char(110,109,101,110,116)..string.char(32,100,101,116,101)..string.char(99,116,101,100), 0) end
+end
+end
+_0x0000()
 local _0x0005 = 4
-  if _0x0005 > 3 then
-  print(string.char(104,101,108,108,111))
+if _0x0005 > 3 then
+print(string.char(104,101,108,108)..string.char(111))
 end
